c7980526ad33bcf7098876db738e67fbd171af0f
JGuzman87 <jguzman913@gmail.com>
2025-10-07 19:48:23 -0400

    created map for nav links

--- src/app/weight/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WeightPage = () => {
-  return (
-    <div>WeightPage</div>
-  )
-}
-
-export default WeightPage;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,18 @@
 import Link from "next/link";
 
 const Nav = () => {
+  const links = ["dashboard", "stats", "meals"];
   return (
     <nav className="bg-blue-100 flex p-2 gap-4">
-      <ul className="flex gap-4 p-4">
+      <ul className="flex gap-4 p-4 capitalize">
         <li>
-          <Link href={"/"}>
-            <p>Home Page</p>
-          </Link>
+          <Link href={"/"}>Home</Link>
         </li>
-        <li>
-          <Link href={"/dashboard"}>
-            <p className="">Dashboard</p>
-          </Link>
-        </li>
-        <li>
-          <Link href={"/body_stats"}>
-            <p>Body Stats</p>
-          </Link>
-        </li>
-
-        <li>
-          <Link href={"/meals"}>
-            <p>Meals</p>
-          </Link>
-        </li>
-        <li>
-          <Link href={"/weight"}>
-            <p>Weight</p>
-          </Link>
-        </li>
+        {links.map((link) => (
+          <li>
+            <Link href={`/${link}`}>{link}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
